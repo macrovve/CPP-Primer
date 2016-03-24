ccc7cdf47a0242fd2f6c84610ae694a81a6b669c
Macrovve <macrovve@gmail.com>
2016-03-24 17:48:00 +0800

    feat: add ex4.28

---
+++ Ch4/4.28.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   cout<<"bool:\t\t"<<sizeof(bool)<<endl;
+   cout<<"char:\t\t"<<sizeof(char)<<endl;
+   cout<<"wchar_t:\t"<<sizeof(wchar_t)<<endl;
+   cout<<"short:\t\t"<<sizeof(short)<<endl;
+   cout<<"int:\t\t"<<sizeof(int)<<endl;
+   cout<<"long:\t\t"<<sizeof(long)<<endl;
+   cout<<"long long:\t"<<sizeof(long)<<endl;
+   cout<<"float:\t\t"<<sizeof(float)<<endl;
+   cout<<"double:\t\t"<<sizeof(double)<<endl;
+   cout<<"long double:\t"<<sizeof(long double)<<endl;
+
+}
